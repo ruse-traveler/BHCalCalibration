7c5db6c27440cd0902bdbd714497613009c5cc61
danderson <danderson@rcf.rhic.bnl.gov>
2022-12-01 15:10:23 -0500

    Include Barrel ECal collections in output

--- RunHCalCalibrator.sh
+++ RunHCalCalibrator.sh
@@ -7,6 +7,6 @@
 # JCalibrateHCal JANA plugin
 
 input="../forHCalClusterCheck_p2d7080n5000pip_d12m11y2022.edm4hep.root"
-collections="HcalBarrelRecHits,HcalBarrelClusters,HcalBarrelTruthClusters,GeneratedParticles"
+collections="HcalBarrelRecHits,EcalBarrelSciGlassRecHits,HcalBarrelClusters,EcalBarrelSciGlassClusters,HcalBarrelTruthClusters,EcalBarrelSciGlassTruthClusters,GeneratedParticles"
 
 eicrecon -Pplugins=JCalibrateHCal -Ppodio:output_include_collections=$collections $input
