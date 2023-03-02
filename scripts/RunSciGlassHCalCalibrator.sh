59155d9570235057735e45281621b2c97216380d
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-03-02 13:02:10 -0500

    Set detector geometry to SciGlass before running

--- scripts/RunSciGlassHCalCalibrator.sh
+++ scripts/RunSciGlassHCalCalibrator.sh
@@ -9,12 +9,15 @@
 # -----------------------------------------------------------------------------
 
 # i/o files
-input="../forVaryingClusterPars.e10th70n10Kpip.d19m1y2023.edm4hep.root"
-podio="forPodioReaderTest_fromEicRecon.e5th70n500pip.d18m1y2023.podio.root"
-output="forPodioReaderTest_fromEicRecon.e5th70n500pip.d18m1y2023.jcalibratehcal.root"
+input="../forECalStudy.sciGlass_run0.e5th35145n25Kpim.d22m2y2023.edm4hep.root"
+podio="test_sciglass.podio.root"
+output="test_sciglass.hists.root"
 
 # output collections from EICrecon
 collections="HcalBarrelRecHits,HcalBarrelClusters,EcalBarrelSciGlassClusters,HcalBarrelIslandProtoClusters,HcalBarrelTruthClusters,HcalBarrelTruthProtoClusters,GeneratedParticles"
+
+# set geometry to SciGlass bemc
+export DETECTOR_CONFIG=epic
 
 eicrecon -Pplugins=JCalibrateHCalWithSciGlass -Ppodio:output_include_collections=$collections -Ppodio:output_file=$podio -Phistsfile=$output $input
 
