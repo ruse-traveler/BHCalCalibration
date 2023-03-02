68b7710f56d6b89c717622f38059d012e3c58f5f
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-03-02 11:48:39 -0500

    Remove merged hits collection & distance parameters

--- scripts/RunHCalCalibrator.sh
+++ scripts/RunHCalCalibrator.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# -----------------------------------------------------------------------------
 # 'RunHCalCalibrator.sh'
 # Derek Anderson
 # 11.07.2022
 #
 # A simple script to run the
 # JCalibrateHCal JANA plugin
+# -----------------------------------------------------------------------------
 
 # i/o files
 input="../forVaryingClusterPars.e10th70n10Kpip.d19m1y2023.edm4hep.root"
@@ -12,15 +14,8 @@ podio="forPodioReaderTest_fromEicRecon.e5th70n500pip.d18m1y2023.podio.root"
 output="forPodioReaderTest_fromEicRecon.e5th70n500pip.d18m1y2023.jcalibratehcal.root"
 
 # output collections from EICrecon
-collections="HcalBarrelRecHits,HcalBarrelMergedHits,HcalBarrelClusters,HcalBarrelIslandProtoClusters,HcalBarrelTruthClusters,HcalBarrelTruthProtoClusters,GeneratedParticles"
+collections="HcalBarrelRecHits,HcalBarrelClusters,HcalBarrelIslandProtoClusters,HcalBarrelTruthClusters,HcalBarrelTruthProtoClusters,GeneratedParticles"
 
-# clustering parameters
-scale="5,5"
-distXY="150,150"
-sector="5"
-center="0.03"
-energy="0.003"
-
-eicrecon -Pplugins=JCalibrateHCal -PHCAL:HcalBarrelIslandProtoClusters:dimScaledLocalDistXY=$scale -PHCAL:HcalBarrelIslandProtoClusters:localDistXY=$distXY -PHCAL:HcalBarrelIslandProtoClusters:sectorDist=$sector -PHCAL:HcalBarrelIslandProtoClusters:minClusterCenterEdep=$center -PHCAL:HcalBarrelIslandProtoClusters:minClusterHitEdep=$energy -Ppodio:output_include_collections=$collections -Ppodio:output_file=$podio -Phistsfile=$output $input
+eicrecon -Pplugins=JCalibrateHCal -Ppodio:output_include_collections=$collections -Ppodio:output_file=$podio -Phistsfile=$output $input
 
 # end -------------------------------------------------------------------------
