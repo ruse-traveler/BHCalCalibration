7c9fafb86382af8c3490719998f844da3f8c0b55
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-03-08 10:15:44 -0500

    Remove debugging statements

--- scripts/RunSingleEICReconForECalStudy.sh
+++ scripts/RunSingleEICReconForECalStudy.sh
@@ -24,17 +24,6 @@ input=$8
 podout=$9
 plugout=${10}
 
-echo "CHECK1  setup    = $setup"
-echo "CHECK2  rundir   = $rundir"
-echo "CHECK3  detector = $detector"
-echo "CHECK4  config   = $config"
-echo "CHECK5  install  = $installdir"
-echo "CHECK6  collect  = $collections"
-echo "CHECK7  plugin   = $plugin"
-echo "CHECK8  input    = $input"
-echo "CHECK9  podout   = $podout"
-echo "CHECK10 plugout  = $plugout"
-
 # set eicrecon
 source $setup
 export PATH=$PATH':/usr/local/bin'
@@ -43,13 +32,9 @@ export PATH=$PATH':/usr/local/bin'
 source $detector
 export DETECTOR_CONFIG=$config
 
-echo "CHECK11 det config = $DETECTOR_CONFIG"
-
 # set install path
 export EICrecon_MY=$installdir
 cd $rundir
-
-echo "CHECK12 install dir = $EICrecon_MY"
 
 # build plugin
 cmake -S $plugin -B $plugin/build
