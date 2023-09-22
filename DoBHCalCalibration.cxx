980e8b86c96b91f8da66c4993afeed4abdf58cb5
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-09-22 10:12:54 -0400

    Continue adding in options to driver macro

--- DoBHCalCalibration.cxx
+++ DoBHCalCalibration.cxx
@@ -36,69 +36,93 @@ void DoBHCalCalibration() {
     "JCalibrateWithImaging/ntToCalibrate"
   };
 
+  // how to use each leaf
+  const enum Usage { Tuple, Train, Target, Specter };
+
   // tuple parameters
-  vector<string> vecTupleVars = {
-    "ePar",
-    "fracParVsLeadBHCal",
-    "fracParVsLeadBEMC",
-    "fracParVsSumBHCal",
-    "fracParVsSumBEMC",
-    "fracLeadBHCalVsBEMC",
-    "fracSumBHCalVsBEMC",
-    "eLeadBHCal",
-    "eLeadBEMC",
-    "eSumBHCal",
-    "eSumBEMC",
-    "diffLeadBHCal",
-    "diffLeadBEMC",
-    "diffSumBHCal",
-    "diffSumBEMC",
-    "nHitsLeadBHCal",
-    "nHitsLeadBEMC",
-    "nClustBHCal",
-    "nClustBEMC",
-    "hLeadBHCal",
-    "hLeadBEMC",
-    "fLeadBHCal",
-    "fLeadBEMC",
-    "eLeadImage",
-    "eSumImage",
-    "eLeadSciFi",
-    "eSumSciFi",
-    "nClustImage",
-    "nClustSciFi",
-    "hLeadImage",
-    "hLeadSciFi",
-    "fLeadImage",
-    "fLeadSciFi",
-    "eSumSciFiLayer1",
-    "eSumSciFiLayer2",
-    "eSumSciFiLayer3",
-    "eSumSciFiLayer4",
-    "eSumSciFiLayer5",
-    "eSumSciFiLayer6",
-    "eSumSciFiLayer7",
-    "eSumSciFiLayer8",
-    "eSumSciFiLayer9",
-    "eSumSciFiLayer10",
-    "eSumSciFiLayer11",
-    "eSumSciFiLayer12",
-    "eSumImageLayer1",
-    "eSumImageLayer2",
-    "eSumImageLayer3",
-    "eSumImageLayer4",
-    "eSumImageLayer5",
-    "eSumImageLayer6"
+  vector<pair<bool, string>> vecTrainAndTupleVars = {
+    {Usage::Target, "ePar"},
+    {Usage::Tuple,  "fracParVsLeadBHCal"},
+    {Usage::Tuple,  "fracParVsLeadBEMC"},
+    {Usage::Tuple,  "fracParVsSumBHCal"},
+    {Usage::Tuple,  "fracParVsSumBEMC"},
+    {Usage::Tuple,  "fracLeadBHCalVsBEMC"},
+    {Usage::Tuple,  "fracSumBHCalVsBEMC"},
+    {Usage::Train,  "eLeadBHCal"},
+    {Usage::Train,  "eLeadBEMC"},
+    {Usage::Tuple,  "eSumBHCal"},
+    {Usage::Tuple,  "eSumBEMC"},
+    {Usage::Tuple,  "diffLeadBHCal"},
+    {Usage::Tuple,  "diffLeadBEMC"},
+    {Usage::Tuple,  "diffSumBHCal"},
+    {Usage::Tuple,  "diffSumBEMC"},
+    {Usage::Train,  "nHitsLeadBHCal"},
+    {Usage::Train,  "nHitsLeadBEMC"},
+    {Usage::Tuple,  "nClustBHCal"},
+    {Usage::Tuple,  "nClustBEMC"},
+    {Usage::Tuple,  "hLeadBHCal"},
+    {Usage::Tuple,  "hLeadBEMC"},
+    {Usage::Tuple,  "fLeadBHCal"},
+    {Usage::Tuple,  "fLeadBEMC"},
+    {Usage::Tuple,  "eLeadImage"},
+    {Usage::Train,  "eSumImage"},
+    {Usage::Tuple,  "eLeadSciFi"},
+    {Usage::Train,  "eSumSciFi"},
+    {Usage::Tuple,  "nClustImage"},
+    {Usage::Tuple,  "nClustSciFi"},
+    {Usage::Tuple,  "hLeadImage"},
+    {Usage::Tuple,  "hLeadSciFi"},
+    {Usage::Tuple,  "fLeadImage"},
+    {Usage::Tuple,  "fLeadSciFi"},
+    {Usage::Train,  "eSumSciFiLayer1"},
+    {Usage::Train,  "eSumSciFiLayer2"},
+    {Usage::Train,  "eSumSciFiLayer3"},
+    {Usage::Train,  "eSumSciFiLayer4"},
+    {Usage::Train,  "eSumSciFiLayer5"},
+    {Usage::Train,  "eSumSciFiLayer6"},
+    {Usage::Train,  "eSumSciFiLayer7"},
+    {Usage::Train,  "eSumSciFiLayer8"},
+    {Usage::Train,  "eSumSciFiLayer9"},
+    {Usage::Train,  "eSumSciFiLayer10"},
+    {Usage::Train,  "eSumSciFiLayer11"},
+    {Usage::Train,  "eSumSciFiLayer12"},
+    {Usage::Train,  "eSumImageLayer1"},
+    {Usage::Train,  "eSumImageLayer2"},
+    {Usage::Train,  "eSumImageLayer3"},
+    {Usage::Train,  "eSumImageLayer4"},
+    {Usage::Train,  "eSumImageLayer5"},
+    {Usage::Train,  "eSumImageLayer6"}
   };
 
   // tmva parameters
   const string sLoader("TMVADir");
   const string sFactory("TMVARegression");
 
+  // sort tuple leaves
+  vector<string> vecTupleLeaves;
+  vector<string> vecTmvaTrainers;
+  vector<string> vecTmvaTargets;
+  vector<string> vecTmvaSpectators;
+  for (const auto trainAndTupleVars : vecTrainAndTupleVars) {
+    switch (trainAndTupleVars.first) {
+      case Usage::Train:
+        vecTmvaTrainers.push_back(trainAndTupleVars.second);
+        break;
+      case Usage::Target:
+        vecTmvaTargets.push_back(trainAndTupleVars.second);
+        break;
+      case Usage::Specter:
+        vecTmvaSpectators.push_back(trainAndTupleVars.second);
+        break;
+    }
+    vecTupleLeaves.push_back(trainAndTupleVars.second);
+  }
+
   // run calibration workflow
   BHCalCalibration* calibrator = new BHCalCalibration(sFactory, sLoader, sOutput);
   calibrator -> SetInput(sInput[0], sInput[1]);
-  calibrator -> SetTupleArgs(vecTupleVars);
+  calibrator -> SetTupleArgs(vecTupleLEaves);
+  calibrator -> SetTmvaArgs(vecTmvaTrainers, vecTmvaTargets, vecTmvaSpectators);
   calibrator -> Init();
   calibrator -> Train();
   calibrator -> Apply();
