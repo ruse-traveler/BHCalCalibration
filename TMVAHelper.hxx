bbe0086cc16139aca74c69d5c5dd445a3dabb014
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-09-17 11:11:32 -0400

    Finish filling in necessary methods

--- TMVAHelper.hxx
+++ TMVAHelper.hxx
@@ -18,6 +18,7 @@
 #include <cassert>
 #include <utility>
 #include <iostream>
+#include <algorithm>
 // root libraries
 #include <TString.h>
 // tmva components
@@ -77,6 +78,7 @@ class TMVAHelper {
     std::vector<std::string> m_vecReadOpt;
 
     // method members
+    std::vector<bool>        m_read;
     std::vector<std::string> m_methods;
 
     // input members
@@ -85,7 +87,9 @@ class TMVAHelper {
     std::vector<std::string> m_targets;
 
     // output members
-    std::vector<std::string> m_outputs;
+    std::vector<float>                 m_out_values;
+    std::vector<std::string>           m_out_variables;
+    std::map<std::string, std::size_t> m_out_index;
 
     // ------------------------------------------------------------------------
     //! Set input variables
@@ -124,23 +128,58 @@ class TMVAHelper {
     inline void SetMethods(const std::vector<std::string>& methods) {
 
       m_methods = methods;
+      m_read.resize( m_methods.size(), true );
       return;
 
     }  // end 'SetMethods(std::vector<std::string>&)'
 
     // ------------------------------------------------------------------------
-    //! Generate list of targets
+    //! Generate list of regression outputs
     // ------------------------------------------------------------------------
+    /*! Will generate list of targets and the regression
+     *  outputs. Every specified target will be evaluated
+     *  for every specified method.
+     */
     inline void GenerateRegressionOutputs() {
 
+      // 1st load targets
+      std::size_t iOut = 0;
+      for (const std::string& target : m_targets) {
+        m_out_variables.push_back(target);
+        m_out_index[m_out_variables.back()] = iOut;
+        ++iOut;
+      }
+
+      // then generate list of regression outputs
       for (const std::string& method : m_methods) {
         for (const std::string& target : m_targets) {
-          m_outputs.push_back(target + "_" + method);
+          m_out_variables.push_back(target + "_" + method);
+          m_out_index[m_out_variables.back()] = iOut;
+          ++iOut;
         }
       }
+
+      // resize values vector acordingly & exit
+      m_out_values.resize(iOut);
       return;
 
     }  // end 'GenerateListOfTargers()'
+
+    // ------------------------------------------------------------------------
+    //! Helper method to check if a file exists or not
+    // ------------------------------------------------------------------------
+    inline bool DoesFileExist(const std::string& name) const {
+
+      bool exists;
+      if (FILE* file = fopen(name.c_str(), "r")) {
+        fclose(file);
+        exists = true;
+      } else {
+        exists = false;
+      }
+      return exists;
+
+    }  // end 'DoesFileExist(std::string)'
 
     // ------------------------------------------------------------------------
     //! Helper method to compress vector of strings into a colon-separated list
@@ -161,6 +200,13 @@ class TMVAHelper {
   public:
 
     // ------------------------------------------------------------------------
+    //! Compress lists into colon-separated ones
+    // ------------------------------------------------------------------------
+    inline std::string CompressReadOptions()    const {return CompressList(m_vecReadOpt);}
+    inline std::string CompressTrainOptions()   const {return CompressList(m_vecTrainOpt);}
+    inline std::string CompressFactoryOptions() const {return CompressList(m_vecFactoryOpt);}
+
+    // ------------------------------------------------------------------------
     //! Getters
     // ------------------------------------------------------------------------
     inline std::vector<std::string> GetFactoryOptions()  const {return m_vecFactoryOpt;}
@@ -170,14 +216,36 @@ class TMVAHelper {
     inline std::vector<std::string> GetSpectatingVars()  const {return m_watchers;}
     inline std::vector<std::string> GetTrainingVars()    const {return m_trainers;}
     inline std::vector<std::string> GetTargetVars()      const {return m_targets;}
-    inline std::vector<std::string> GetOutputs()         const {return m_outputs;}
+    inline std::vector<std::string> GetOutVars()         const {return m_out_variables;}
 
     // ------------------------------------------------------------------------
-    //! Compress lists into colon-separated ones
+    //! Get a specific output variable
     // ------------------------------------------------------------------------
-    inline std::string CompressReadOptions()    const {return CompressList(m_vecReadOpt);}
-    inline std::string CompressTrainOptions()   const {return CompressList(m_vecTrainOpt);}
-    inline std::string CompressFactoryOptions() const {return CompressList(m_vecFactoryOpt);}
+    inline float GetVariable(const std::string& var) {
+
+      // check if variable exists
+      if (!m_out_index.count(var)) {
+        assert(m_out_index.count(var));
+      }
+
+      // then get variable
+      return m_out_values.at(m_out_index[var]);
+
+    }  // end 'GetVariable(std::string&)'
+
+    // ------------------------------------------------------------------------
+    //! Reset output values
+    // ------------------------------------------------------------------------
+    inline void ResetValues() {
+
+      std::fill(
+        m_out_values.begin(),
+        m_out_values.end(),
+        -1. * std::numeric_limits<float>::max()
+      );
+      return;
+
+    }  // end 'ResetValues()'
 
     // ------------------------------------------------------------------------
     //! Setters
@@ -214,7 +282,7 @@ class TMVAHelper {
     // ------------------------------------------------------------------------
     //! Add NTuple variables to reader
     // ------------------------------------------------------------------------
-    inline void ReadVariables(TMVA::Reader* reader, NTupleHelper& helper) {
+    inline void ReadVariables(TMVA::Reader* reader, NTupleHelper& helper) const {
 
       for (const std::string& train : m_trainers) {
         if (!helper.m_index.count(train)) {
@@ -253,35 +321,95 @@ class TMVAHelper {
      *  by `directory`. If the file exists, helper will book the method to be
      *  evaulated with that file.
      */
-    inline void BookMethodsToRead(TMVA::Reader* reader, const std::string& directory) {
+    inline void BookMethodsToRead(TMVA::Reader* reader, const std::string& directory, const std::string& name) {
 
-      // lambda to check if a file exists or not
-      auto exists = [](const std::string& name) {
-        if (FILE* file = fopen(name.c_str(), "r")) {
-          fclose(file);
-          return true;
-        } else {
-          return false;
-        }
-      };
+      // loop over all methods
+      for (std::size_t iMethod = 0; iMethod < m_methods.size(); ++iMethod) {
 
-      for (const std::string& method : m_methods) {
+        // construct full path
+        const std::string path = directory + "/weights/" + name + "_" + m_methods[iMethod] + ".weights.xml";
 
-        // construct full path, skip if file does not exist
-        const std::string path = directory + "/" + method + ".weights.xml";
-        if (!exists(path)) {
+        // skip if file does not exist
+        if (!DoesFileExist(path)) {
           std::cerr << "WARNING: file '" << path << "' doesn't exist! Not booking method!" << std::endl;
+          m_read.at(iMethod) = false;
           continue;
         }
 
         // otherwise, construct title and book method
-        const std::string title = method + " method";
+        const std::string title = m_methods[iMethod] + " method";
         reader -> BookMVA(title, path);
 
+      }  // end method loop
+      return;
+
+    }  // end 'BookMethodsToRead(TMVA::Reader*, std::string&, std::string&)'
+
+    // ------------------------------------------------------------------------
+    //! Book methods to read by providing a list of weight files
+    // ------------------------------------------------------------------------
+    /*! For file passed, helper wil check if file exists and, if it does,
+     *  book the corresponding method to be evaluated with that file. This
+     *  assumes that the insertion order is the same between the methods
+     *  list and the provided list of files.
+     */
+    inline void BookMethodsToRead(TMVA::Reader* reader, const std::vector<std::string>& files) {
+
+      // make sure input list has same dimension as method list
+      if (files.size() != m_methods.size()) {
+        assert(files.size() == m_methods.size());
+      }
+
+      // loop over provided files
+      for (std::size_t iFile = 0; iFile < files.size(); ++iFile) {
+
+        // skip if file does not exist
+        if (!DoesFileExist(files[iFile])) {
+          std::cerr << "WARNING: file '" << files[iFile] << "' doesn't exist! Not booking method '" << m_methods.at(iFile) <<"'!" << std::endl;
+          m_read.at(iFile) = false;
+          continue;
+        }
+
+        // otherwise, construct title and book method
+        const std::string title = m_methods.at(iFile) + " method";
+        reader -> BookMVA(title, files[iFile]);
+
+      }  // end file loop
+      return;
+
+    }  // end 'BookMethodsToRead(TMVA::Reader*, std::vector<std::string>&)'
+
+    // ------------------------------------------------------------------------
+    //! Evaluate all booked methods
+    // ------------------------------------------------------------------------
+    inline void EvaluateMethods(TMVA::Reader* reader, NTupleHelper& helper) {
+
+      // loop over all methods
+      for (std::size_t iMethod = 0; iMethod < m_methods.size(); ++iMethod) {
+
+        // if not evaluating method, continue
+        if (!m_read[iMethod]) {
+          continue;
+        }
+
+        // construct title & run evaluation
+        const std::string        title   = m_methods[iMethod] + " method";
+        const std::vector<float> targets = reader -> EvaluateRegression(title);
+
+        // collect regression output
+        for (std::size_t iTarget = 0; iTarget < m_targets.size(); ++iTarget) {
+          const std::string output = m_targets[iTarget] + "_" + m_methods[iMethod];
+          m_out_values.at(m_out_index[output]) = targets.at(iTarget);
+        }
+      }
+
+      // then collect training targets in output
+      for (const std::string& target : m_targets) {
+        m_out_values.at(m_out_index[target]) = helper.GetVariable( target );
       }
       return;
 
-    }  // end 'BookMethodsToRead(TMVA::Reader*, directory)'
+    }  // end 'EvaluateMethods(TMVA::Reader*, NTupleHelper&)'
 
     // ------------------------------------------------------------------------
     //! Default ctor/dtor
